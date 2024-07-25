3e006a751f2ae1016deda1567e2927ba7c4b5072
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-25 13:53:31 +0800

    fix: 修复路径不是执行处的问题

--- 开发工具/git/git连续尝试.py
+++ 开发工具/git/git连续尝试.py
@@ -9,8 +9,6 @@ from colorama import init, Fore
 
 # --- init ---
 init(autoreset=True)
-script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-os.chdir(script_path)
 # ------------
 
 def run_commits(working_dir, command):
@@ -38,7 +36,8 @@ def main():
     result = subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if result.returncode == 0:
-        working_dir = script_path
+        working_dir = os.getcwd()
+        print(f"\r{Fore.GREEN}✓{Fore.RESET} 已自动选择仓库目录: {working_dir}")
     else:
         root = tk.Tk()
         root.withdraw()
