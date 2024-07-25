7520eaf45b666c1f20539d2674e1dad129847b37
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-25 13:34:43 +0800

    pref: 在某个仓库中执行git相关自动化时可以省去选择仓库目录的步骤

--- 开发工具/git/git连续尝试.py
+++ 开发工具/git/git连续尝试.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import time
 import subprocess
 import tkinter as tk
@@ -9,6 +11,8 @@ from colorama import init, Fore
 init(autoreset=True)
 root = tk.Tk()
 root.withdraw()
+script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+os.chdir(script_path)
 # ------------
 
 def run_commits(working_dir, command):
@@ -33,9 +37,14 @@ def is_network_error(stderr): # 判断错误类型
     return False
 
 def main():
-    print(f"{Fore.BLUE}?{Fore.RESET} 请选择仓库目录", end="")
-    working_dir = filedialog.askdirectory()
-    print(f"\r{Fore.GREEN}✓{Fore.RESET} 选择的仓库目录: {working_dir}")
+    result = subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if result.returncode == 0:
+        working_dir = script_path
+    else:
+        print(f"{Fore.BLUE}?{Fore.RESET} 请选择仓库目录", end="")
+        working_dir = filedialog.askdirectory()
+        print(f"\r{Fore.GREEN}✓{Fore.RESET} 选择的仓库目录: {working_dir}")
     
     command = input("请输入需要执行的命令: ")
 
