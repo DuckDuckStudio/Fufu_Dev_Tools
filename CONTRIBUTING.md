0829491d89f2f7e40a699ecc7735b54016e6236e
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-31 21:22:04 +0800

    sync: 同步更改

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -2,29 +2,36 @@
 
 在贡献前请先阅读[LICENSE许可文件](https://github.com/DuckDuckStudio/Fufu_Tools/blob/main/LICENSE)。  
 
-# 提交建议 / 直接修改
+## 提交建议 / 直接修改
 您可以通过在仓库的[Issues](https://github.com/DuckDuckStudio/yazicbs.github.io/issues)界面提交您的建议，您也可以Fork本仓库后直接修改并提交 PR。  
 请确保您的修改没有与其他PR重复或者无效后再提交PR。  
 
-## 不会处理的 PR
-- 进行大范围的误导性修改的
-- 破坏目录夹架构的
-- 含非法内容的
-- 无效的
-- 重复的
-- 影响未在PR中提到的内容的
-- 其他不合适的PR 
+## 关于打包
+### [Fufu_Dev_Tools](https://github.com/DuckDuckStudio/Fufu_Dev_Tools) 仓库
+1. 手动克隆仓库到本地  
+   ```powershell
+   git clone https://github.com/DuckDuckStudio/Fufu_Dev_Tools.git
+   ```
+2. 下载芙芙工具箱的[发行版](https://github.com/DuckDuckStudio/Fufu_Tools/releases)或克隆最新[仓库](https://github.com/DuckDuckStudio/Fufu_Tools)  
+   ```powershell
+   winget install DuckStudio.FufuTools #发行版
+   git clone https://github.com/DuckDuckStudio/Fufu_Tools.git #仓库
+   ```
+3. 构建虚拟环境
+   ```powershell
+   python -m venv .venv
+   & ".venv\Scripts\Activate.ps1"
+   ```
+4. 运行芙芙工具箱的自动打包工具(`Tools\【实验性工具】\自动化\自动打包所有py文件\使用Pyinstaller.py`)  
+5. 输入相关参数
+<!--等待补充-->
 
-## 不会处理的 Issues
-- 无效的
-- 重复的
-
-# 关于翻译
+## 关于翻译
 你可以对本项目进行翻译，请将翻译后的文件放在`翻译版`>`{tag}`下。  
 如果你没找到`翻译版`文件夹就在根目录自己建一个。  
 
 <div style="text-align: right;">
     <p>鸭鸭「カモ」</p>
-    <p>2024年5月22日</p>
-    <p>编辑内容：初始文档</p>
+    <p>2024年7月31日</p>
+    <p>编辑内容：更新打包过程</p>
 </div>
---
+++ 开发工具-源码/Script/ffdev.ps1
@@ -0,0 +1,44 @@
+param(
+    [string]$program,
+    [string]$command,
+    [string[]]$inputArgs
+)
+
+$parentDir = Split-Path -Path $PSScriptRoot -Parent
+$version = "2024.07.25.2030"
+$flag = 0
+
+if ($program -eq "总调用") {
+    $pythonScript = Join-Path $parentDir "总调用.py"
+} elseif ($program -eq "目录复制") {
+    $pythonScript = Join-Path $parentDir "目录复制.py"
+} elseif ($program -eq "参数查重") {
+    $pythonScript = Join-Path $parentDir "代码校对\参数查重.py"
+} elseif ($program -eq "非UTF-8编码") {
+    $pythonScript = Join-Path $parentDir "代码校对\非UTF-8编码.py"
+} elseif ($program -eq "尾随空格") {
+    $pythonScript = Join-Path $parentDir "代码校对\尾随空格.py"
+} elseif ($program -eq "需求生成") {
+    $pythonScript = Join-Path $parentDir "生成工具\需求生成.py"
+} elseif ($program -eq "代码行数") {
+    $pythonScript = Join-Path $parentDir "统计\代码总行数.py"
+} elseif ($program -eq "账号切换") {
+    $pythonScript = Join-Path $parentDir "git\账号切换.py"
+} elseif ($program -eq "连续push") {
+    $pythonScript = Join-Path $parentDir "git\连续push尝试.py"
+} elseif ($program -eq "连续pull") {
+    $pythonScript = Join-Path $parentDir "git\连续pull尝试.py"
+} elseif ($program -eq "git连续尝试") {
+    $pythonScript = Join-Path $parentDir "git\git连续尝试.py"
+} elseif ($program -in "ver", "版本", "version", "Version", "--version", "--ver", "-v") {
+    Write-Output "Version: $version"
+    $flag = 1
+} else {
+    Write-Warning "无效的程序调用"
+    Write-Output "可用程序: [目录复制] [参数查重] [非UTF-8编码] [尾随空格] [需求生成] [代码行数] [账号切换] [连续push] [连续pull] [git连续尝试]"
+    $flag = 1
+}
+
+if ($flag -ne 1) {
+    python $pythonScript $command $inputArgs
+}
